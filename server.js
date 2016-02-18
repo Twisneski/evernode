3a5e8ad887364cf79417e983fd09d6554f1fafe7
Twisneski <tina.wisneski525@gmail.com>
2016-02-18 11:38:20 -0600

    added route model

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const note = require('./routes/note');
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = require('./models/note');
-// const Note = mongoose.model('Notes', mongoose.Schema({
-//   title: String,
-//   text: String
-// }));refactor to note.js model
 
 app.set('view engine', 'jade');
 //middleware come before routes
@@ -23,27 +19,8 @@ app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
+app.use(note);
 
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-});
-//serves form - Note is just a function with a method on it
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-});
-//this is a redirect
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
 
