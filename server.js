1c9a333f4b7c4aa339727844810129e42b382b32
Twisneski <tina.wisneski525@gmail.com>
2016-02-18 10:53:15 -0600

    save new note through the db

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 
 app.set('view engine', 'jade');
 //middleware come before routes
@@ -21,16 +25,23 @@ app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
 //serves form
-
 app.post('/notes', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note)
+    res.redirect('/');
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
 });
-//starts express
+//start express then add mongoose. wrap mongoose around listen function
+
 
 
 
