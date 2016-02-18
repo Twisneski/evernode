aee1ffd05ee2f56da40f939a7785d54c16c96ff8
Twisneski <tina.wisneski525@gmail.com>
2016-02-18 10:35:33 -0600

    first feature

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-//express
-//mongoose
-//body parser
-
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+//middleware come before routes
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/', (req, res) => {
   res.send('Server Running');
+});
+
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+//serves form
+
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
 });
+//starts express
+
+
+
 
